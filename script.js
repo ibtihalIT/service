92634416c07803babb2c8490c2e35e99163b75ee
ibtihalIT <153672785+ibtihalIT@users.noreply.github.com>
2024-04-24 21:34:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showDetail(vehicleId) {
+    alert('You clicked on: ' + vehicleId);
+    // Here you can expand this function to show more detailed content dynamically
+    // For instance, opening a modal or redirecting to another page with more details
+}
